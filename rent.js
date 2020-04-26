c7ab3b9f7fdac8ba92ba9db2227d0d767a4fc879
V1perW0lf <christopher.42.ellis@gmail.com>
2020-04-26 18:11:12 -0500

    final for now

--- rent.js
+++ rent.js
@@ -14,18 +14,13 @@ const server = http.createServer((request, response) => {
     body = Buffer.concat(body).toString();
     const length = "Total Amount: ".length
     const chargeAmountBegInd = body.indexOf("Total Amount:") + length;
-    console.log("beg ind: " + chargeAmountBegInd);
     const chargeAmountEndInd = body.indexOf("*RealPage Payments Services LLC - 2201 Lakeside Blvd., Richardson, TX 75082") - 4;
-    console.log("end ind: " + chargeAmountEndInd);
     const chargeAmount = parseFloat(body.substring(chargeAmountBegInd + 1, chargeAmountEndInd).replace(",", ""));
-    console.log("before parsefloat: " + body.substring(chargeAmountBegInd + 1, chargeAmountEndInd))
-    console.log("after parsefloat: " + chargeAmount)
     const internetAmountPerPerson = 21;
     const petFee = 50;
     const electricityTotal = 140;
     const electricityPerPerson = electricityTotal / 2;
     const blakePayPal = (chargeAmount + electricityTotal) - (((chargeAmount - petFee) / 2) + internetAmountPerPerson + electricityPerPerson);
-    console.log("blakepaypal: " + blakePayPal)
     client.messages
     .create({
       body: 'Hey Blake! This is how much you should PayPal CJ: $' + blakePayPal,
