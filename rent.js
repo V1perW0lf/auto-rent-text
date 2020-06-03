e964dc79e47863c194b16e6f10ca9a02b5cf7e60
Christopher Ellis <christopher.42.ellis@gmail.com>
2020-06-03 17:12:53 -0500

    Added route to update electricity amount

--- rent.js
+++ rent.js
@@ -10,27 +10,35 @@ const server = http.createServer((request, response) => {
     let body = [];
     request.on('data', (chunk) => {
       body.push(chunk);
-  }).on('end', () => {
-    body = Buffer.concat(body).toString();
-    const length = "Total Amount: ".length
-    const chargeAmountBegInd = body.indexOf("Total Amount:") + length;
-    const chargeAmountEndInd = body.indexOf("*RealPage Payments Services LLC - 2201 Lakeside Blvd., Richardson, TX 75082") - 2;
-    const chargeAmount = parseFloat(body.substring(chargeAmountBegInd + 1, chargeAmountEndInd).replace(",", ""));
-    const internetAmountPerPerson = 21;
-    const petFee = 50;
-    const electricityTotal = 151;
-    const electricityPerPerson = electricityTotal / 2;
-    const blakePayPal = ((chargeAmount + electricityTotal) - (((chargeAmount - petFee) / 2) + internetAmountPerPerson + electricityPerPerson)).toFixed(2);
-    client.messages
-    .create({
-      body: 'Hey Blake! This is how much you should PayPal CJ: $' + blakePayPal,
-      from: process.env.twilioNumber,
-      to: process.env.blakeNumber
+    }).on('end', () => {
+      body = Buffer.concat(body).toString();
+      const length = "Total Amount: ".length
+      const chargeAmountBegInd = body.indexOf("Total Amount:") + length;
+      const chargeAmountEndInd = body.indexOf("*RealPage Payments Services LLC - 2201 Lakeside Blvd., Richardson, TX 75082") - 2;
+      const chargeAmount = parseFloat(body.substring(chargeAmountBegInd + 1, chargeAmountEndInd).replace(",", ""));
+      const internetAmountPerPerson = 21;
+      const petFee = 50;
+      const electricityTotal = 151;
+      const electricityPerPerson = electricityTotal / 2;
+      const blakePayPal = ((chargeAmount + electricityTotal) - (((chargeAmount - petFee) / 2) + internetAmountPerPerson + electricityPerPerson)).toFixed(2);
+      client.messages
+      .create({
+        body: 'Hey Blake! This is how much you should PayPal CJ: $' + blakePayPal,
+        from: process.env.twilioNumber,
+        to: process.env.blakeNumber
+      })
+      response.write(blakePayPal.toString());
+      response.end();
     })
-    response.write(blakePayPal.toString());
-    response.end();
+  }
+  if (request.method === "POST" && request.url === "/calc/electricity") {
+    let body = [];
+    request.on('data', (chunk) => {
+      body.push(chunk);
+    }).on('end', () => {
+      body = Buffer.concat(body).toString();
+      process.env.electricity = body;
   })
-}
 });
 
 server.listen(process.env.PORT || 5000);
