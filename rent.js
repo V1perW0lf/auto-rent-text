833a3de0f066cac0e2d133bc527614c377457ea3
Christopher Ellis <christopher.42.ellis@gmail.com>
2020-08-01 12:07:40 -0500

    chargeAmount test

--- rent.js
+++ rent.js
@@ -13,7 +13,7 @@ const server = http.createServer((request, response) => {
     }).on('end', () => {
       body = Buffer.concat(body).toString();
       const length = "Total Amount: ".length
-      const chargeAmountBegInd = body.indexOf("Total Amount:") + length;
+      const chargeAmountBegInd = body.indexOf("Total Amount:");// + length;
       const chargeAmountEndInd = chargeAmountBegInd + 20;
       const chargeAmount = body.substring(chargeAmountBegInd + 1, chargeAmountEndInd); //.replace(",", "");
       const internetAmountPerPerson = 21;
@@ -23,7 +23,7 @@ const server = http.createServer((request, response) => {
       const blakePayPal = ((chargeAmount + electricityTotal) - (((chargeAmount - petFee) / 2) + internetAmountPerPerson + electricityPerPerson)).toFixed(2);
       client.messages
       .create({
-        body: 'Hey Blake! This is how much you should PayPal CJ: $' + body,
+        body: 'Hey Blake! This is how much you should PayPal CJ: $' + chargeAmount + ".",
         from: process.env.twilioNumber,
         to: process.env.blakeNumber
       })
